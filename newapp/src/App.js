7ba8dfd735e00a75c380405edc5215a9cbdc9501
Augusto Reis <bilisreis@outlook.com>
2025-08-08 23:12:54 -0300

    Enviando o trabalho

---
+++ newapp/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+import twins from "./assets/img/twins.JPG"
+import ricardo from "./assets/img/ricardo.png.jpg"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Gustavo e Augusto</h1>
+      <p>Bad boys</p>
+      <p>Casa da vó Wanda e vô Eduardo</p>
+      <img class="img" src={twins} title="guris da catequese" />
+      <h1>Ricardo</h1>
+      <p>30 anos</p>
+      <p>Rua Odorico Mosmann - Centro</p>
+      <img class="img" src={ricardo} />
+    </div>
+  );
+}
+
+export default App;
